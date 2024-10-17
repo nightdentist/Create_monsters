36ec00659f562dc3cd72dddf82bda633b95df8a2
nightdentist <67285412+nightdentist@users.noreply.github.com>
2024-10-17 23:20:34 +0300

    next_commit

--- main.py
+++ main.py
@@ -19,8 +19,17 @@ context = {
     "luck" : "5",
     "skill_1" : "Навык 1",
     "skill_2" : "Навык 2",
-    "skill_3" : "Навык 3"
+    "skill_3" : "Навык 3",
   }
+skills = ["Стремительный прыжок",
+          "Электрический выстрел",
+          "Ледяной удар",
+          "Стремительный удар",
+          "Кислотный взгляд",
+          "Тайный побег",
+          "Ледяной выстрел",
+          "Огненный заряд"
+          ]
 
 context["first_name"] = fake.first_name()
 context["last_name"] = fake.last_name()
@@ -31,6 +40,11 @@ context["agility"] = random.randint(3, 18)
 context["endurance"] = random.randint(3, 18)
 context["intelligence"] = random.randint(3, 18)
 context["luck"] = random.randint(3, 18)
+context["skill_1"] = random.choice(skills)
+context["skill_2"] = random.choice(skills)
+context["skill_3"] = random.choice(skills)
+
+
 
 
 file_operations.render_template("charsheet.svg", "result.svg", context)
