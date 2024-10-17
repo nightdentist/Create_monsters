5dc6675b1a2e468952020701380929777224f3f1
nightdentist <67285412+nightdentist@users.noreply.github.com>
2024-10-17 16:34:07 +0300

    second_commit

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-from file_operations import VERSION
+from faker import Faker
 
-print(VERSION)
+import random
+
+import file_operations
+
+
+fake = Faker("ru_RU")
+
+context = {
+    "first_name": "Евсемпий",
+    "last_name": "Пупидрович",
+    "job": "Пархутехтор",
+    "town" : "Маркувиль",
+    "strength" : "5",
+    "agility" : "5",
+    "endurance" : "5",
+    "intelligence" : "5",
+    "luck" : "5",
+    "skill_1" : "Навык 1",
+    "skill_2" : "Навык 2",
+    "skill_3" : "Навык 3"
+  }
+
+context["first_name"] = fake.first_name()
+context["last_name"] = fake.last_name()
+context["job"] = fake.job()
+context["town"] = fake.city()
+context["strength"] = random.randint(3, 18)
+context["agility"] = random.randint(3, 18)
+context["endurance"] = random.randint(3, 18)
+context["intelligence"] = random.randint(3, 18)
+context["luck"] = random.randint(3, 18)
+
+
+file_operations.render_template("charsheet.svg", "result.svg", context)
